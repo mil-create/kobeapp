a73520a546919600c7540995a6e3cae8eee77978
mil-create <dauzonnn@gmail.com>
2025-10-03 10:09:18 +0800

    signup

--- koebapp/src/app/pages/signup/signup.page.ts
+++ koebapp/src/app/pages/signup/signup.page.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class SignupPage implements OnInit {
+  formData = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    console.log('SignupPage initialized');
   }
 
+  handleSubmit() {
+    console.log('Sign up form submitted:', this.formData);
+    
+    // Validation
+    if (!this.formData.name || !this.formData.email || !this.formData.password) {
+      alert('Please fill in all fields!');
+      return;
+    }
+    
+    if (this.formData.password !== this.formData.confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+    
+    if (this.formData.password.length < 6) {
+      alert('Password must be at least 6 characters!');
+      return;
+    }
+    
+    alert('Sign up successful! Mamba Mentality activated.');
+    // Navigate to sign in or home page
+    // this.router.navigate(['/signin']);
+  }
+
+  goToSignIn() {
+    this.router.navigate(['/signin']);
+  }
 }
